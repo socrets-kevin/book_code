f1745839244f00dd24df5615e26fccb6e64a0c06
socrets <fullstack_dev@126.com>
2016-03-29 21:59:22 +0800

    test shell sort

--- KR_C/shellsort.c
+++ KR_C/shellsort.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+
+void shell_sort(int v[], int n);
+
+int main()
+{
+    int v[] = {2, 5, 3, 10};
+    shell_sort(v, 4);
+    for (int i = 0; i < 4; i++) {
+	printf("%d\n", v[i]);
+    }
+}
+
 void shell_sort(int v[], int n) {
     int temp, i, j, gap;
 
